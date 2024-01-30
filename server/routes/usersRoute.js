9de3f59ef0e32f704bdf93d668cf859412744958
Tharusha-2000 <112651385+Tharusha-2000@users.noreply.github.com>
2024-01-30 11:05:15 +0530

    add tharusha's backend

---
+++ server/routes/usersRoute.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "User with this email already exists." });
+    }
+
+    const newUser = new User({ email, password });
+    const user = await newUser.save();
+    res.send("User Registered Successfully");
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+  try{
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "User does not exist"});
+    }
+  
+    if (password !== user.password) {
+      return res.status(400).json({ error: "Invalid credentials." });
+    }
+  
+    res.send("User Logged In Successfully");
+  }
+  
+  catch (error) {
+    return res.status(400).json({ error });
+  }
+  
+  } );
+  
+  module.exports = router;
---
+++ server/utils/db.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require("mongoose");
+mongoose.set('strictQuery', true);
+
+var mongoURL = 'mongodb+srv://tharusha:tharusha123@cluster0.blcsaij.mongodb.net/'
+
+mongoose.connect(process.env.MONGODB_URI || mongoURL, { useUnifiedTopology: true, useNewUrlParser: true, })
+
+
+var connection = mongoose.connection
+
+connection.on('error', () => {
+    console.log('MongDB Connection Failed')
+})
+
+connection.on('connected', () => {
+    console.log('MongoDB Connection Successful')
+})
+
+module.exports = mongoose
